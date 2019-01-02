81e657a34665ac9399eb9383f1dfcc580112aaa5
Ling Long <ling.long@mail.utoronto.ca>
2019-01-01 23:23:15 -0700

    added tictactoe game skeleton

---
+++ pupper.js
@@ -0,0 +1,11 @@
+
+
+function move(square){
+    var id = square.toString();
+    var current = document.getElementById(id);
+    current.style.backgroundColor = "rgb(51, 153, 255)";
+}; 
+
+function gameOver(grid){
+
+};
